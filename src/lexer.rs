e534992404910009f908f5d2523926fb2c8462bf
Michelle S <alterae@protonmail.com>
2022-02-28 14:24:26 -0700

    Add some tokens
    We can now lex the example file without error.

--- src/lexer.rs
+++ src/lexer.rs
@@ -4,6 +4,41 @@
 /// Token is an atomic unit of source code.
 #[derive(logos::Logos, Debug, PartialEq)]
 pub enum Token {
+    #[regex(r"[_a-zA-Z][_0-9a-zA-Z]*")]
+    Identifier,
+
+    // keywords
+    #[regex(r"use")]
+    Use,
+    #[regex(r"fn")]
+    Fn,
+
+    // literals
+    #[regex(r#""(?:[^"]|\\")*""#)] // scuffed quotes lol
+    String,
+    #[regex(r"[1-9][_1-9]*")] // todo: support for non-integers
+    Number,
+
+    // parenthesis, curly braces
+    #[regex(r"\(")]
+    ParenLeft,
+    #[regex(r"\)")]
+    ParenRight,
+    #[regex(r"\{")]
+    BraceLeft,
+    #[regex(r"\}")]
+    BraceRight,
+
+    // other punctuation
+    #[regex(r":")]
+    Colon,
+    #[regex(r"::")]
+    ColonColon,
+    #[regex(r"=")]
+    Equal,
+    #[regex(r"==")]
+    EqualEqual,
+
     #[error]
     #[regex(r"[ \t\n\f]+", logos::skip)]
     #[regex(r"//[^\n]*", logos::skip)]
