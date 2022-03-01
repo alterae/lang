9a92abf500bfc2a8f9fbb4b101347cd3a7d6bdc7
Michelle S <alterae@protonmail.com>
2022-03-01 14:20:10 -0700

    Move `TokenStream` to `lexer` module
    It's closely coupled with the `Token` type and this lets us enforce the API usage in the parser

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,7 +2,7 @@
 //! of tokens.
 
 /// Token is an atomic unit of source code.
-#[derive(logos::Logos, Copy, Debug, PartialEq)]
+#[derive(logos::Logos, Copy, Clone, Debug, PartialEq)]
 pub enum Token {
     #[regex(r"[_a-zA-Z][_0-9a-zA-Z]*")]
     Identifier,
@@ -43,4 +43,48 @@ pub enum Token {
     #[regex(r"\s+", logos::skip)]
     #[regex(r"//[^\n]*", logos::skip)]
     Error,
+}
+
+// TokenStream holds state for the token stream.
+pub struct TokenStream {
+    // The stream of tokens to parse.
+    _stream: Vec<Token>,
+    // The index of the token currently being parsed.
+    _current: usize,
+}
+
+impl TokenStream {
+    // New constructs a new token stream.
+    pub fn new(stream: Vec<Token>) -> Self {
+        Self {
+            _stream: stream,
+            _current: 0,
+        }
+    }
+
+    // Current gets the current token in the stream.
+    pub fn _current(&self) -> Token {
+        self._stream[self._current]
+    }
+
+    // Next gets the next token in the stream.
+    pub fn _next(&self) -> Token {
+        self._stream[self._current + 1]
+    }
+
+    // Expect checks if the next token is equal to t. If it is, the token is consumed and returned.
+    pub fn _expect(&mut self, t: Token) -> Option<Token> {
+        if self._current() == t {
+            Some(self._advance())
+        } else {
+            None
+        }
+    }
+
+    // Advances returns the current token, and advances the current index.
+    pub fn _advance(&mut self) -> Token {
+        let result = self._current();
+        self._current += 1;
+        result
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ fn main() {
 
     // read the entire file into memory, which isn't great for large programs
     // but whatever
-    let tokens: Vec<_> = match fs::read_to_string(&opts.path) {
+    let stream = match fs::read_to_string(&opts.path) {
         Err(e) => {
             eprintln!("Error opening {:?}: {e}", opts.path);
             process::exit(1)
         }
-        Ok(code) => lexer::Token::lexer(&code).collect(),
+        Ok(code) => lexer::TokenStream::new(lexer::Token::lexer(&code).collect()),
     };
 
-    let _ = parser::parse(tokens);
+    let _ = parser::parse(stream);
 }
