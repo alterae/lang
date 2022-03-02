266c7c63f11e11f1b419d04b410b69bf8a73c21b
Michelle S <alterae@protonmail.com>
2022-03-02 14:26:27 -0700

    Undo lots of code
    It all sucks.  Need to take a step back and re-evaluate.

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,13 @@ fn main() {
 
     // read the entire file into memory, which isn't great for large programs
     // but whatever
-    let stream = match fs::read_to_string(&opts.path) {
+    let _stream = match fs::read_to_string(&opts.path) {
         Err(e) => {
             eprintln!("Error opening {:?}: {e}", opts.path);
             process::exit(1)
         }
-        Ok(code) => lexer::TokenStream::new(lexer::Token::lexer(&code).collect()),
+        Ok(code) => lexer::Token::lexer(&code).collect(),
     };
 
-    let _ = parser::parse(stream);
+    // todo: parsing
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,4 @@
 //! Module parser contains code to turn flat token streams into abstract
 //! syntax trees.
 
-use crate::lexer;
-
-/// Ast is an abstract syntax tree.
-pub struct Ast {}
-
-/// Parse converts a stream of tokens into an abstract syntax tree.
-pub fn parse(_stream: lexer::TokenStream) -> Ast {
-    Ast {}
-}
+// TODO: aaaaaaaaaaaaaa
